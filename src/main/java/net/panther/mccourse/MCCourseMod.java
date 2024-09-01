26d11f057be7b7a16dc1f1ef6d00d5089be921f1
Panther Kabanosikoder <kflodab@gmail.com>
2024-09-01 13:57:51 +0200

    Added pink garnet but not rendering?

--- src/main/java/net/panther/mccourse/MCCourseMod.java
+++ src/main/java/net/panther/mccourse/MCCourseMod.java
@@ -2,6 +2,7 @@ package net.panther.mccourse;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.panther.mccourse.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,9 +10,8 @@ public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
