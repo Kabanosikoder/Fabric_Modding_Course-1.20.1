d47df0c094e8ca47f0c2cf867ab9cf30d43bb62a
Panther Kabanosikoder <kflodab@gmail.com>
2024-09-09 06:10:54 +0200

    Added data gen and deleted all unnecessary json files

--- src/main/java/net/panther/mccourse/MCCourseModDataGenerator.java
+++ src/main/java/net/panther/mccourse/MCCourseModDataGenerator.java
@@ -2,10 +2,16 @@ package net.panther.mccourse;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.panther.mccourse.datagen.*;
 
 public class MCCourseModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModBlockLootTableGenerator::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeGenerator::new);
 	}
 }
